b61d610202f6e7538826b24c9d1ad51bfaaedace
Samet ARTUL <smtartul@gmail.com>
2022-01-09 20:14:01 +0300

    react nested urls has applied

--- react/reactApp/react5routing/src/index.js
+++ react/reactApp/react5routing/src/index.js
@@ -12,9 +12,10 @@ const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="about" element={<About />} />
-      <Route path="users" element={<Users />} />
+      <Route path="/" element={<App />} >
+        <Route path="about" element={<About />} />
+        <Route path="users" element={<Users />} />
+      </Route>
     </Routes>
   </BrowserRouter>,
   rootElement
